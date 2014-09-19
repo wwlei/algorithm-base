1ce3837d380276697fd125ccb2cce27ef9b745bc
wwlei <archive.document.data@gmail.com>
2014-09-19 16:01:14 +0800

    4

--- base/src/main/java/algorithm/App.java
+++ base/src/main/java/algorithm/App.java
@@ -9,8 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.pritnln( "test second pull");
-        System.out.pritnln( "test 3 pull");
-        System.out.pritnln( "test 4 pull");
+ 
+        System.out.println( "test second pull");
+        
+        System.out.println( "test 5 pull");
+ 
+        System.out.println( "test 3 pull");
+        System.out.println( "test 4 pull");
+ 
     }
 }
